b90a4a920cb4cee8adc174d1e8295c635289e953
edris <soltani.edris@gmail.com>
2023-06-12 14:09:48 +0330

    first

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <template>
-  <DxDataGrid
-      :data-source="drivers">
-      <DxColumn data-field="busID">
-          <DxLookup
-              :data-source="buses"
-              value-expr="busID"
-              display-expr="plates"
-          />
-      </DxColumn>
+  <DxDataGrid :data-source="drivers">
+    <DxColumn data-field="busID">
+      <DxLookup :data-source="buses" value-expr="busID" display-expr="plates" />
+    </DxColumn>
   </DxDataGrid>
 </template>
 
@@ -23,12 +18,14 @@ const drivers = [
   { driverID: 1, firstName: "John", lastName: "Smith", busID: 2 },
   { driverID: 2, firstName: "Lizzy", lastName: "Cook", busID: 1 },
   { driverID: 3, firstName: "Brian", lastName: "Hawkins", busID: 3 }
+  { driverID: 4, firstName: "edris", lastName: "soltani", busID: 4 }
 ];
 
 const buses = [
   { busID: 1, plates: "123456" },
   { busID: 2, plates: "AB-1234" },
-  { busID: 3, plates: "CD-9876" }
+  { busID: 3, plates: "CD-9876" },
+  { busID: 4, plates: "CD-98ss76" }
 ];
 
 export default {
