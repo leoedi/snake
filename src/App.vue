436503de142837abdc54b73ba2ecbf942226eeba
edris <soltani.edris@gmail.com>
2023-06-12 14:12:39 +0330

    das

--- src/App.vue
+++ src/App.vue
@@ -7,38 +7,35 @@
 </template>
 
 <script>
-import 'devextreme/dist/css/dx.light.css';
+import "devextreme/dist/css/dx.light.css";
 
-import {DxDataGrid,
-  DxColumn,
-  DxLookup
-} from 'devextreme-vue/data-grid';
+import { DxDataGrid, DxColumn, DxLookup } from "devextreme-vue/data-grid";
 
 const drivers = [
   { driverID: 1, firstName: "John", lastName: "Smith", busID: 2 },
   { driverID: 2, firstName: "Lizzy", lastName: "Cook", busID: 1 },
-  { driverID: 3, firstName: "Brian", lastName: "Hawkins", busID: 3 }
-  { driverID: 4, firstName: "edris", lastName: "soltani", busID: 4 }
+  { driverID: 3, firstName: "Brian", lastName: "Hawkins", busID: 3 },
+  { driverID: 4, firstName: "edris", lastName: "soltani", busID: 4 },
 ];
 
 const buses = [
   { busID: 1, plates: "123456" },
   { busID: 2, plates: "AB-1234" },
   { busID: 3, plates: "CD-9876" },
-  { busID: 4, plates: "CD-98ss76" }
+  { busID: 4, plates: "CD-98ss76" },
 ];
 
 export default {
   components: {
-      DxDataGrid,
-      DxColumn,
-      DxLookup
+    DxDataGrid,
+    DxColumn,
+    DxLookup,
   },
   data() {
-      return {
-          drivers,
-          buses
-      }
-  }
-}
+    return {
+      drivers,
+      buses,
+    };
+  },
+};
 </script>
